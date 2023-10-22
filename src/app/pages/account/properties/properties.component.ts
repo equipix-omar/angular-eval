b8f1b2b59f9f811d5bef245fc3bd22f42d6ee761
Muhammed Abdurahman <mu.abdurahman93@gmail.com>
2023-10-22 04:36:47 +0300

    fix in-progress orders tab

--- src/app/pages/account/properties/properties.component.ts
+++ src/app/pages/account/properties/properties.component.ts
@@ -1,38 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { properties } from './properties.model';
-import { propertiesData } from './data';
-import {HelperService} from "../../../core/services/helper.service";
-import {OrderApiService} from "../../../core/services/Api-Services/order.api.service";
-import { OrderItem, OrderItemModel} from "../../../core/models/classes/order.item.model";
+import { properties } from "./properties.model";
+import { propertiesData } from "./data";
+import { HelperService } from "../../../core/services/helper.service";
+import { OrderApiService } from "../../../core/services/Api-Services/order.api.service";
+import {
+  OrderItem,
+  OrderItemModel,
+} from "../../../core/models/classes/order.item.model";
 
 @Component({
-  selector: 'app-properties',
-  templateUrl: './properties.component.html',
-  styleUrls: ['./properties.component.scss']
+  selector: "app-properties",
+  templateUrl: "./properties.component.html",
+  styleUrls: ["./properties.component.scss"],
 })
 
 /**
  * Properties Component
  */
 export class PropertiesComponent implements OnInit {
-
   // bread crumb items
   breadCrumbItems!: Array<{}>;
   propertiesData!: properties[];
-  order !: OrderItemModel ;
-  orders !:OrderItem[];
-  constructor(protected helperService: HelperService,
-              private  orderApiService:OrderApiService) { }
+  order!: OrderItemModel;
+  orders!: OrderItem[];
+  constructor(
+    protected helperService: HelperService,
+    private orderApiService: OrderApiService
+  ) {}
 
   ngOnInit(): void {
     /**
      * BreadCrumb
      */
-     this.breadCrumbItems = [
-      { label: 'Home', link:'' },
-      { label: 'Account', link:'/account/info' },
-      { label: 'My Properties', active: true }
+    this.breadCrumbItems = [
+      { label: "Home", link: "" },
+      { label: "Account", link: "/account/info" },
+      { label: "My Properties", active: true },
     ];
 
     // Chat Data Get Function
@@ -40,32 +44,32 @@ export class PropertiesComponent implements OnInit {
     this._fetchAllOrdersData();
   }
 
-
-
-   // Chat Data Fetch
-   private _fetchData() {
+  // Chat Data Fetch
+  private _fetchData() {
     this.propertiesData = propertiesData;
   }
 
-  _fetchAllOrdersData(){
-      this.orderApiService._getAllOrdersData().subscribe({
-        next:(resp)=>{
-          console.log("order apiiiiiiiiiii ",resp);
-          this.order=resp;
-          this.orders =this.order.body;
-          console.log("order api ",this.orders);
-
-        },error:(error) =>{
-          let error_message = error?.error?.message;
-            console.log("error of order api",error)
-        }
-      });
-  };
+  _fetchAllOrdersData() {
+    this.orderApiService._getAllOrdersData().subscribe({
+      next: (resp) => {
+        console.log("order apiiiiiiiiiii ", resp);
+        this.order = resp;
+        // get only in-progress orders
+        this.orders = this.order.body.filter((order) => {
+          return order.status.key === "SUBMITTED";
+        });
+        console.log("order api ", this.orders);
+      },
+      error: (error) => {
+        let error_message = error?.error?.message;
+        console.log("error of order api", error);
+      },
+    });
+  }
   /**
    * On mobile toggle button clicked
    */
-   SideBarMenu() {
-    document.getElementById('account-nav')?.classList.toggle('show');
+  SideBarMenu() {
+    document.getElementById("account-nav")?.classList.toggle("show");
   }
-
 }
